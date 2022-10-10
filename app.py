b9d3039445f0e37029a3aa1750b6950030e69649
Oskar30 <85636221+Oskar30@users.noreply.github.com>
2022-10-10 21:13:44 +0400

    add post_update and del

--- app.py
+++ app.py
@@ -17,20 +17,54 @@ class Article(db.Model):
     def __repr__(self):
         return '<Article %r>' % self.id
 
+
 @app.route('/')
 @app.route('/home')
 def index():
     return render_template('index.html')
+
 
 @app.route('/posts')
 def posts():
     articles = Article.query.order_by(Article.date.desc()).all()
     return render_template('posts.html', articles=articles)
 
+
 @app.route('/posts/<int:id>')
 def post_detail(id):
     article = Article.query.get(id)
     return render_template('post_detail.html', article=article)
+
+
+@app.route('/posts/<int:id>/delete')
+def post_delete(id):
+    article = Article.query.get_or_404(id)
+    
+    try:
+        db.session.delete(article)
+        db.session.commit()
+        return redirect('/posts')
+    except:
+        return 'При удалении статьи произошла ошибка' 
+
+
+@app.route('/posts/<int:id>/update', methods=['POST','GET'])
+def post_update(id):
+    article = Article.query.get(id)
+    if request.method == 'POST':
+        article.title =request.form['title']
+        article.intro =request.form['intro']
+        article.text =request.form['text']
+
+        try:
+            db.session.commit()
+            return redirect('/posts')
+        except:
+            return 'При редактировании статьи произошла ошибка'
+
+    else:        
+        return render_template('post_update.html', article=article)
+
 
 @app.route('/create_article', methods=['POST','GET'])
 def create_article():
@@ -46,14 +80,16 @@ def create_article():
             db.session.commit()
             return redirect('/posts')
         except:
-            return 'Ошибка'
+            return 'При добавлениии статьи произошла ошибка'
 
     else:
         return render_template('create_article.html')
 
+
 @app.route('/user/<string:name>/<int:id>')
 def user(name, id):
     return 'User page: ' + name + ' - ' + str(id)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
